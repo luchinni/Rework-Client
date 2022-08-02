236d8236116a3cc6ea5f613515f0f7c8ca77f6a9
Gabriel <gabi300709@gmail.com>
2022-08-02 18:53:52 -0300

    update(*): searchBar input added

---
+++ src/Components/Nav/Nav.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SearchBar from './SearchBar/SearchBar'
+
+const Nav = () => {
+  return (
+    <div>
+        <SearchBar/>
+    </div>
+  )
+}
+
+export default Nav
