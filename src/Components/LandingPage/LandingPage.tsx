ddc27fd640f145e1c70464cf9d03b3cb3f97c255
Gabriel <gabi300709@gmail.com>
2022-08-03 17:44:35 -0300

    correcion de commit

--- src/Components/LandingPage/LandingPage.tsx
+++ src/Components/LandingPage/LandingPage.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-// import Header from '../Header/Header';
 
 const LandingPage = () => {
   return (
     <div>
-      {/* <Header /> */}
       <main><h1>HOLA</h1></main>
     </div>
   )
