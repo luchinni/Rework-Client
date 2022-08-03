e587366f311e50fdfe8f53a712d7522086a0f5d9
villalb4 <villalb4angel@gmail.com>
2022-08-03 20:09:42 -0300

    Add(landing)styles

--- src/Components/LandingPage/Top/Top.tsx
+++ src/Components/LandingPage/Top/Top.tsx
@@ -7,8 +7,8 @@ function Top() {
   return (
     <div className='Top_component'>
       <div className='Top_divIzquierda'>
-        <h1 className='Top_h1'>Conectamos tus nececidades con los mejores profesionales</h1>
-        <p className='Top_pageDescription'>Publica un proyecto y comienza a recibir propuestas Aplica a los proyectos que mejor se adapten a tus habilidades</p>
+        <h1 className='Top_h1'>Conectamos tus <span className='h1_nececidad'>nececidades</span> con los mejores <span className='h1_profesionales'>profesionales</span></h1>
+        <p className='Top_pageDescription'>Publica un proyecto y comienza a recibir propuestas <br /> Aplica a los proyectos que mejor se adapten a tus habilidades</p>
         <Link to="/home" className='Top_buttonAcceder'>Prueba gratis</Link>
       </div>
       <div className='Top_divImage'>
