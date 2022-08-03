9c60b00e75643df4c960a012da9e5f1f4d3487f7
Gabriel <gabi300709@gmail.com>
2022-08-03 17:41:16 -0300

    routes(app): first routes

--- src/Components/LandingPage/LandingPage.tsx
+++ src/Components/LandingPage/LandingPage.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Header from '../Header/Header';
+// import Header from '../Header/Header';
 
 const LandingPage = () => {
   return (
     <div>
-      <Header />
-      <main></main>
+      {/* <Header /> */}
+      <main><h1>HOLA</h1></main>
     </div>
   )
 }
