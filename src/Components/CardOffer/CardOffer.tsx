09a598aa020cf2e613d86653cb8f45f6df70b56f
Gabriel <gabi300709@gmail.com>
2022-08-03 21:50:14 -0300

    renderizacion de cards funcionando

--- src/Components/CardOffer/CardOffer.tsx
+++ src/Components/CardOffer/CardOffer.tsx
@@ -1,26 +1,26 @@
 import React from 'react'
 import './CardOffer.css';
 
-const CardOffer = (props:any) => {
+const CardOffer = ({props}:any) => {
+
   return (
     <div>
       <div>
         <span>info client</span>
-        <img className='image' src="" alt="Foto usuario" />
         <div>
           <img src={props.photoClient} alt="Client Photo" />
-          <span>{props.name}</span>
-          <span>{props.rating}</span>
+          <span>{props.name}</span><br/>
+          <span>{props.rating}</span><br/>
         </div>
       </div>
       <div>
-        <span>info offer</span>
-        <span>{props.title}</span>
-        <span>{props.remuneration}</span>
-        <span>{props.work_duration_time}</span>
-        <p>{props.description}</p>
+        <span>info offer</span><br/>
+        <span>{props.title}</span><br/>
+        <span>{props.remuneration.join(' - ')}</span><br/>
+        <span>{props.work_duration_time}</span><br/>
+        <p>{props.description}</p><br/>
         <img src={props.photo} alt="Offer Photo" />
-        <span>{props.tags}</span>
+        <span>{props.tags.join(', ')}</span>
       </div>
       <button>detail</button>
     </div>
