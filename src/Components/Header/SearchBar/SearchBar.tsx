ee91e9f576c4733311171722e06c5ffe9439021a
villalb4 <villalb4angel@gmail.com>
2022-08-03 23:14:51 -0300

    Add(*) Styles and icons for landing page

--- src/Components/Header/SearchBar/SearchBar.tsx
+++ src/Components/Header/SearchBar/SearchBar.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React , {useState} from 'react';
+import './SearchBar.css';
+import icon_filter from '../../../images/icon_filters.svg';
+import icon_search from '../../../images/icon_search.svg';
 
 const SearchBar = () => {
+  const [open, setOpen] = useState(false)
+
+  function handleClick() {
+    setOpen(!open)
+  }
+
   return (
-    <div>
-        <input type="text" placeholder='Busca usuario/trabajo' />
+    <div className='SearchBar_component'>
+      <div className='SearchBar'>
+        <img className='icon_search' src={icon_search} alt="search" />
+        <input className='input_search' type="text" placeholder='Busca usuario/trabajo' />
+        <button className='button_filters' onClick={handleClick}><img className='icon_filters' src={icon_filter} alt="filters" /></button>
+      </div>
+      {open &&
+        <div className='filter_dropDown'>
+          <div className='filter_option'>
+            <label>Usuarios</label>
+            <input type="checkbox" />
+          </div>
+          <div className='filter_option'>
+            <label>Trabajo</label>
+            <input type="checkbox" />
+          </div>
+        </div>
+      }
     </div>
   )
 }
--- src/Components/LandingPage/Top/Top.tsx
+++ src/Components/LandingPage/Top/Top.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import hero from '../../../images/hero.png';
 import "./Top.css"
+import newTab from "../../../images/openNewTab.png"
 
 function Top() {
   return (
     <div className='Top_component'>
       <div className='Top_divIzquierda'>
-        <h1 className='Top_h1'>Conectamos tus <span className='h1_nececidad'>nececidades</span> con los mejores <span className='h1_profesionales'>profesionales</span></h1>
+        <h1 className='Top_h1'>Conectamos tu <span className='h1_nececidad'>nececidad</span> con los mejores <span className='h1_profesionales'>profesionales</span></h1>
         <p className='Top_pageDescription'>Publica un proyecto y comienza a recibir propuestas <br /> Aplica a los proyectos que mejor se adapten a tus habilidades</p>
-        <Link to="/home" className='Top_buttonAcceder'>Prueba gratis</Link>
+        <Link to="/home" className='Top_buttonAcceder'>Prueba gratis<img className='new_tabIcon' src={newTab} alt="" /></Link>
       </div>
       <div className='Top_divImage'>
         <img src={hero} alt="hero" />
