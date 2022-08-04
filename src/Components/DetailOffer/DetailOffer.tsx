2e843e8e55ecacaa4efd1b20fa113db77d124816
Gabriel <gabi300709@gmail.com>
2022-08-04 17:21:16 -0300

    detailOffer ya muestra la info

--- src/Components/DetailOffer/DetailOffer.tsx
+++ src/Components/DetailOffer/DetailOffer.tsx
@@ -23,26 +23,26 @@ const DetailOffer = () => {
         <p>info Client</p>
         <img src={offerId.photoClient} alt="fotito" />
         <p>{offerId.name}</p>
-        <p>{offerId.rating}</p>
+        <p>Rating {offerId.rating}</p>
       </div>
       <div>
         <div><h2>info Offer</h2></div>
         <h2>{offerId.title}</h2>
-        <p>{offerId.remuneration}</p>
-        <p>{offerId.work_duration_time}</p>
+        <p>{`Paga estimada ARS: ${offerId.remuneration?.slice(0, 1)} - ${offerId.remuneration?.slice(1)}`}</p>
+        <p>{`Tiempo estimado del trabajo: ${offerId.work_duration_time}`}</p>
         <p>{offerId.description}</p>
         <img src={offerId.photo} alt="fotito offer"/>
-        <p>{offerId.tags.join(', ')}</p>
+        <p>{offerId.tags?.join(', ')}</p>
       </div>
       <div>
         <div><h2>info Proposals</h2></div>
-        <div>{offerId?.map((e:any)=>{
+        <div>{offerId.proposals?.map((e:any)=>{
           return (
             <div>
-              <span>{e.name}</span>
-              <span>{e.remunerationProposal}</span>
-              <span>{e.proposal_description}</span>
-              <span>{e.worked_time}</span>
+              <p>{e.name}</p>
+              <p>{`Presupuesto ARS: ${e.remunerationProposal}`}</p>
+              <p>{e.proposal_description}</p>
+              <p>{`Tiempo estimado de entrega: ${e.worked_time}`}</p>
             </div>
           )
         })}</div>
