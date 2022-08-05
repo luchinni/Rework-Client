e43b488b6d3a88f896bdd70b570d30c0df41fa2d
villalb4 <villalb4angel@gmail.com>
2022-08-05 02:13:04 -0300

    Add(*) Updated Styles

--- src/Components/DetailOffer/DetailOffer.tsx
+++ src/Components/DetailOffer/DetailOffer.tsx
@@ -1,13 +1,11 @@
-import React from 'react'
+import React from 'react';
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import {getOfferId} from '../../Redux/Reducer/reducer';
 import Header from '../Header/Header';
+import './DetailOffer.css';
 
 const DetailOffer = () => {
-
-  
 
   const offerId = useSelector((state:any) => state.workService.offerById);
 
@@ -17,38 +15,52 @@ const DetailOffer = () => {
     dispatch(getOfferId());
   }, [dispatch])
 
-
   return (
-    <div>
+    <div className='Detail_component'>
       <Header/>
-      <div>
-        <p>info Client</p>
-        <img src={offerId.photoClient} alt="fotito" />
-        <p>{offerId.name}</p>
-        <p>Rating {offerId.rating}</p>
+      <div className='Detail_detail'>
+
+        <div>
+          <div className='Detail_User'>
+            <div className='Detail_divUserImage'>
+              <img className='Detail_userPhoto' src={offerId.photoClient} alt="fotito" />
+            </div>
+            <p className='Detail_NameUserPost'>{offerId.name}</p>
+            <p className='Detail_UserRating'>Rating {offerId.rating}</p>
+          </div>
+        </div>
+    
+        <div className='Detail_infoProposal'>
+          <div className='Detail_offer'>
+            <div className='Detail_titleTime'>
+              <h2 className='Detail_title'>{offerId.title}</h2>
+              <p className='Detail_time'>{`Tiempo estimado del trabajo: ${offerId.work_duration_time}`}</p>
+            </div>
+            <p className='Detail_remuneration'>{`Paga estimada ARS: ${offerId.remuneration?.slice(0, 1)} - ${offerId.remuneration?.slice(1)}`}</p>
+            <p className='Detail_description'>{offerId.description}</p>
+            <div className='Detail_divImages'>
+              <img className='Detail_images' src={offerId.photo} alt="fotito offer"/>
+            </div>
+            <p className='Detail_tags'>{offerId.tags?.join(', ')}</p>
+            <button className='Detail_buttonApply'>Aplicar</button>
+          </div>
+        </div>
       </div>
-      <div>
-        <div><h2>info Offer</h2></div>
-        <h2>{offerId.title}</h2>
-        <p>{`Paga estimada ARS: ${offerId.remuneration?.slice(0, 1)} - ${offerId.remuneration?.slice(1)}`}</p>
-        <p>{`Tiempo estimado del trabajo: ${offerId.work_duration_time}`}</p>
-        <p>{offerId.description}</p>
-        <img src={offerId.photo} alt="fotito offer"/>
-        <p>{offerId.tags?.join(', ')}</p>
-      </div>
-      <div>
-        <div><h2>info Proposals</h2></div>
-        <div>{offerId.proposals?.map((e:any)=>{
+      <h2 className='Detail_h2Propuestas'>propuestas</h2>
+      <div className='Detail_divProposal'>
+        {offerId.proposals?.map((e:any)=>{
           return (
-            <div>
-              <p>{e.name}</p>
-              <p>{`Presupuesto ARS: ${e.remunerationProposal}`}</p>
-              <p>{e.proposal_description}</p>
-              <p>{`Tiempo estimado de entrega: ${e.worked_time}`}</p>
+            <div className='Detail_Proposal'>
+              <p className='DetailP_UserName'>{e.name}</p>
+              <p className='DetailP_remuneration'>{`Presupuesto ARS: ${e.remunerationProposal}`}</p>
+              <p className='DetailP_propuestaUser'>{e.proposal_description}</p>
+              <p className='DetailP_timeUser'>{`Tiempo estimado de entrega: ${e.worked_time}`}</p>
             </div>
           )
-        })}</div>
+        })}
       </div>
+
+        
     </div>
 
   )
