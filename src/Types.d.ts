432c12889a4492a32481d61cd2d496a66115420c
lucas alejandro pardieux <lucaspardieux@gmail.com>
2022-08-05 01:05:45 -0300

    Feature (Register/client | Redux/Reducer) : Formulario de registro para Client TOTALMENTE funcional

--- src/Types.d.ts
+++ src/Types.d.ts
@@ -10,6 +10,15 @@ export interface ClientType {
     disabled:boolean
 }
 
+export interface newClientType {
+    name:String,
+    lastName:String,
+    password:String,
+    user_mail:String,
+    birthdate:String,
+    image:String,
+}
+
 export interface errorsType {
     name:String,
     lastName:String,
