3de1972148b507a22b313dc4046f987b3055243b
Gabriel <gabi300709@gmail.com>
2022-08-14 03:40:02 -0300

    preparando la accion y boton para una propuesta aceptada

--- src/Components/proposals/CardProposal/CardProposal.tsx
+++ src/Components/proposals/CardProposal/CardProposal.tsx
@@ -1,16 +1,33 @@
 import React from 'react'
 import '../../Offer/DetailOffer/DetailOffer.css';
+import { useDispatch } from 'react-redux';
+const dispatch = useDispatch();
+import { acceptProposal } from '../../../Redux/Reducer/reducer';
+
+const CardProposal = ({props, offer}:any) => {
+  console.log("la offer que llegan a card: ", offer)
+  console.log("las props que llegan a card: ", props)
 
 
-const CardProposal = ({props}:any) => {
-  console.log(props)
+ const handleClick = () => {
+  props.state = "acepted";
+  const state = props.state
+  const id = props.idProposal
+  const proposalState:object = {
+    state,
+    id
+  }
+  dispatch(acceptProposal(proposalState))
+
+  }
+
   return (
     <div className='Detail_Proposal'>              
                 <p className='DetailP_UserName'>{props.userWorker?.name}</p>
                 <p className='DetailP_remuneration'>{`Presupuesto ARS: ${props?.remuneration}`}</p>
                 <p className='DetailP_propuestaUser'>{props?.proposal_description}</p>
                 <p className='DetailP_timeUser'>{`Tiempo estimado de entrega: ${props?.worked_time}`}</p>
-                <button name="button"></button>
+                <button name="button" onClick={handleClick}>Aceptar</button>
               </div>
   )
 }
