dea406955a042dfc29110ddbede9214bcdec5415
villalb4 <villalb4angel@gmail.com>
2022-08-15 19:38:09 -0300

    Add

--- src/Redux/Reducer/reducer.ts
+++ src/Redux/Reducer/reducer.ts
@@ -312,7 +312,7 @@ export const postNewOffer = async (newOffer: type.newOfferType) => {
 export const getOffers = () => async (dispatch: Dispatch<any>) => {
   let pagination = { multiplier: 2 };
   try {
-    const offers = await axios.get("http://localhost:3001/offer?multiplier=50" || "https://rework.up.railway.app/offer?multiplier=50");
+    const offers = await axios.get("https://rework.up.railway.app/offer?multiplier=50" || "http://localhost:3001/offer?multiplier=50");
     // axios({
     //   method:"get",
     //   url: "http://localhost:3001/offer/",
