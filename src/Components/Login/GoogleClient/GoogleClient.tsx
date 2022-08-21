ea9d528ffcc0493c21354730194cec18d17308a7
Franco Fraiese <86889319+francofraiese@users.noreply.github.com>
2022-08-20 23:41:46 -0300

    cambios de registros

--- src/Components/Login/GoogleClient/GoogleClient.tsx
+++ src/Components/Login/GoogleClient/GoogleClient.tsx
@@ -4,12 +4,14 @@ import HeaderRegister from '../../Register/HeaderRegister/HeaderRegister';
 import * as type from "../../../Types";
 import Axios, {AxiosResponse}  from "axios";
 import Swal from 'sweetalert2'
+import { createGoogleClient } from '../../../Redux/Reducer/reducer';
 
 
 const GoogleClient = () => {
     //Requerimos la data de google del estado global y dividimos el nombre completo en nombre y apellido
     const dispatch = useDispatch()
-    const googleData: any = useSelector((state: any) => state.workService.googleData)
+    const localData: any = localStorage.getItem("googleToken")
+    const googleData: any = JSON.parse(localData)
     let name: string = ''
     let lastname: string = ''
     const splitedName: any = googleData.name.split(' ')
@@ -28,8 +30,8 @@ const GoogleClient = () => {
     const [user, setUser] = useState({
         name: name,
         lastName: lastname,
-        password: "",
-        user_mail: googleData.email,
+        password: googleData.password,
+        user_mail: googleData.user_mail,
         birthdate: "",
         image: googleData.photo,
         errors: {
@@ -38,72 +40,71 @@ const GoogleClient = () => {
         disabled: false
     })
 
+    function validarForm(errors: any) {
+      let valid = true;
+      Object.values(errors).forEach(
+        (val: any) => val.length > 0 && (valid = false)
+      );
+      if (valid) {
+        setUser({
+          ...user,
+          disabled: false,
+        });
+      } else {
+        setUser({
+          ...user,
+          disabled: true,
+        });
+      }
+    }
+
     function handleChange(e: any) {
         const value = e.target.value;
         const name = e.target.name;
         let errors: any;
-        errors = user.errors;
-    
-    switch (name){
-        case "birthdate":
-            let fechas = value;
-            let year = fechas.split("-");
-            let date = new Date();
-            let dateNow =
-            date.getFullYear() +
-            "-" +
-            0 +
-            (date.getMonth() + 1) +
-            "-" +
-            0 +
-            date.getDate();
-            console.log(dateNow);
-            console.log(fechas);
-            errors.birthdate =
-            dateNow < fechas
-                ? "La fecha ingresada es inválida."
-                : year[0] > date.getFullYear()
-                ? "La fecha ingresada es inválida."
-                : year[0] < 1940
-                ? "El año debe ser mayor a 1940"
-                : "";
-        break
-        default:
-        break;
-    }
-    function validarForm(errors: any) {
-
-        let valid = true;
-        Object.values(errors).forEach(
-          (val: any) => val.length > 0 && (valid = false)
-        );
-        if (valid) {
-          setUser({
+        errors = user.errors;    
+        switch (name){
+            case "birthdate":
+                let fechas = value;
+                let year = fechas.split("-");
+                let date = new Date();
+                let dateNow =
+                date.getFullYear() +
+                "-" +
+                0 +
+                (date.getMonth() + 1) +
+                "-" +
+                0 +
+                date.getDate();
+                console.log(dateNow);
+                console.log(fechas);
+                errors.birthdate =
+                dateNow < fechas
+                    ? "La fecha ingresada es inválida."
+                    : year[0] > date.getFullYear()
+                    ? "La fecha ingresada es inválida."
+                    : year[0] < 1940
+                    ? "El año debe ser mayor a 1940"
+                    : "";
+                break
+            default:
+                break;
+              }
+        validarForm(user.errors);
+        setUser({
             ...user,
-            disabled: false,
-          });
-        } else {
-          setUser({
-            ...user,
-            disabled: true,
-          });
-        }
-      }
-
-    setUser({
-        ...user,
-        [name]: value,
-        errors,
-      });
-      validarForm(user.errors);
-
+            [name]: value,
+            errors,
+        });
     }
+
     async function postImageOnCloudinary(e: any) {
         const formData = new FormData();
         formData.append("file", e);
         formData.append("upload_preset", "re-work");
     
         try {
+          console.log(formData)
           const response: AxiosResponse = await Axios.post("https://api.cloudinary.com/v1_1/luis-tourn/image/upload", formData);
           const data: any = response.data;
           return data.url;
@@ -113,8 +114,9 @@ const GoogleClient = () => {
       }
     async function handleSubmit(e: any) {
         e.preventDefault();
-        let image = await postImageOnCloudinary(user.image);
-    
+        console.log("el estado", user)
+        let image = await postImageOnCloudinary(user?.image);
+        console.log("imagen ya cargada", image)
         let { name, lastName, password, user_mail, birthdate } = user;
     
         const newClient = {
@@ -125,10 +127,15 @@ const GoogleClient = () => {
           born_date: birthdate,
           photo: image,
         };
-         //hacer action en reducir para registrar al client enviando newClient como param
-        //postNewClient(newClient);
+
+        //hacer action en reducir para registrar al client enviando newClient como param
+
+        dispatch(createGoogleClient(newClient));
+
         let form = document.getElementById("form") as HTMLFormElement | null;
         form?.reset();
+
+        console.log("the client" , newClient)
         Swal.fire("¡Cuenta creada!","Te llegará un correo con tu contraseña provisoria, puedes cambiarla cuando quieras.","success")
       }
 
@@ -139,7 +146,8 @@ const GoogleClient = () => {
                 <h3 className='GoogleClient_title'>¡Un último paso!</h3>
                 <h4 className='GoogleClient_subtitle'>Necesitamos corroborar que eres mayor de edad para continuar,<br/> por favor indícanos tu fecha de nacimiento</h4>
             </div>
-            <div className="GoogleClient_inputAndError">
+            <div className="GoogleClient_form">
+              <form>
                 <input
                   className="GoogleClient_inputDate"
                   type="date"
@@ -160,6 +168,7 @@ const GoogleClient = () => {
                 value="Registrar"
                 onClick={(e) => handleSubmit(e)}
                 />
+              </form>
             </div>
         </div>
     )
--- src/Components/Login/GoogleWorker/GoogleWorker.tsx
+++ src/Components/Login/GoogleWorker/GoogleWorker.tsx
@@ -1,12 +1,142 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import HeaderRegister from '../../Register/HeaderRegister/HeaderRegister';
-
-
+import * as type from "../../../Types";
+import Axios, {AxiosResponse}  from "axios";
+import Swal from 'sweetalert2'
 
 const GoogleWorker = () => {
+    
+    const dispatch = useDispatch()
+    const localData: any = localStorage.getItem("googleToken")
+    const googleData: any = JSON.parse(localData)
+
+    let name: string = ''
+    let lastname: string = ''
+    const splitedName: any = googleData.name.split(' ')
+    
+    if (splitedName.length === 3) {
+        name = splitedName[0] + splitedName[1]
+        lastname = splitedName[2]
+    } else if (splitedName.length === 2) {
+        name = splitedName[0]
+        lastname = splitedName[1]
+    } else if (splitedName.length === 4) {
+        name = splitedName[0] + splitedName[1]
+        lastname = splitedName[2] + splitedName[3]
+    }
+
+    const [user, setUser] = useState({
+        name: name,
+        lastName: lastname,
+        password: "",
+        user_mail: googleData.user_mail,
+        birthdate: "",
+        image: googleData.photo,
+        errors: {
+            birthdate: " ",
+        },
+        disabled: false
+    })
+
+
     return (
-        <div>
+        <div className='GoogleWorker_component'>
             <HeaderRegister/>
-            <span>Hola esto es worker</span>
+            <div className='GoogleWorker_titleContainer'>
+                <h3 className='GoogleWorker_title'>¡Un último paso!</h3>
+                <h4 className='GoogleWorker_subtitle'>Necesitamos corroborar que eres mayor de edad para continuar,<br/> por favor indícanos tu fecha de nacimiento</h4>
+            </div>
+            <div className='Worker_registerDivForm'>
+            <form className='Worker_registerForm' id='form' onSubmit={(e) => e.preventDefault()}>
+              <h1 className='Worker_empecemos'>Empecemos</h1>
+              <div className='Worker_names'>
+                <div className='Worker_nameInput'>
+                  <label>Nombres</label>
+                  <input type="text" name="name" placeholder='Nombre' onChange={(e) => handleChange(e)}/>
+                  {!user.errors.name ? null : <div className='Worker_error'>{user.errors.name}</div>}
+                </div>
+                <div className='Worker_lastnameInput'>
+                  <label>Apellidos</label>
+                  <input type="text" name="lastName" placeholder='Apellido' onChange={(e) => handleChange(e)}/>
+                  {!user.errors.lastName ? null : <div className='Worker_error'>{user.errors.lastName}</div>}
+                </div>
+              </div>
+              <div className='Worker_passwordDate'>
+                <div className='Worker_pass'>
+                  <label>Contraseña</label>
+                  <input type="password" name="password" placeholder='Contraseña' onChange={(e) => handleChange(e)}/>
+                  {!user.errors.password ? <div className='Worker_br'/> : <div className='Worker_errorPw'>{user.errors.password}</div>}
+                </div>
+                <div className="CR_Div_inputAndError">
+                <input
+                  className="CR_inpunt"
+                  type="password"
+                  name="password2"
+                  placeholder="Repita contraseña"
+                  onChange={(e) => handleChange(e)}
+                />
+                {!user.errors.password2 ? null : (
+                  <div className="CR_inputError">
+                    {user.errors.password2}
+                  </div>
+                )}
+              </div>
+                <div className='Worker_date'>
+                  <label>Fecha de nacimiento</label>
+                    <input type="date" name="birthdate" placeholder='Fecha de Nacimiento' onChange={(e) => handleChange(e)}/>
+                    {!user.errors.birthdate ? null : <div className='Worker_error'>{user.errors.birthdate}</div>}
+                </div>
+              </div>
+              <div className='Worker_mailImage'>
+                <div className='Worker_mail'>
+                  <label>Email</label>
+                  <input type="email" name="user_mail" placeholder='E-mail' onChange={(e) => handleChange(e)}/>
+                  {!user.errors.user_mail ? null : <div className='Worker_error'>{user.errors.user_mail}</div>}
+                </div>
+                <div className='Worker_image'>
+                  <label>¡Carga tu foto!</label>
+                  <input className='Worker_image' type="file" name="image" placeholder='carga una imagen de perfil' accept="image/*" onChange={(e) => handleChange(e)}/>
+                  {!user.errors.image ? null : <div className='Worker_error'>{user.errors.image}</div>}
+                </div>
+              </div>
+              <div className='Worker_bothInputs'>
+                <div className='Worker_profession'>
+                  <select name="profession" id='profession' onChange={(e) => handleSelect(e)}>
+                    <option selected={true} hidden>Profesiones</option>
+                    {
+                      user.professions?.map((e:any) =>{
+                          return <option value={e} key={e}> {e} </option>
+                      })
+                    }
+                  </select>
+                  <div className='profession_div'>
+                    {user.profession?.map((e:any) => {
+                      return (<span className='profession_btn' id="profession" key={e} onClick = {(e) => handleDelete(e)}>{`${e}`}</span>)
+                    })}
+                  </div>
+                </div>
+                <div className='Worker_skills'>
+                  <select  name="skills" id='skills' onChange={(e) => handleSelect(e)}>
+                      <option selected={true} hidden>Habilidades</option>
+                      {
+                        user.skills?.map((e:any) =>{
+                            return <option value={e} key={e}> {e} </option>
+                        })
+                      }
+                  </select>
+                  <div className='skills_div'>
+                    {user.skills?.map((e:any) => {
+                      return (<span className='skills_btn' id="skills" key={e} onClick = {(e) => handleDelete(e)}>{`${e}`}</span>)
+                    })}
+                  </div> 
+                </div>
+              </div>
+            </form>
+            <div className='Worker_submit'>
+              <input  disabled={user.disabled} name="button" type="submit" value="¡Registrate!" onClick={(e) => handleSubmit(e)} />
+            </div>
+          </div>
         </div>
     )
 }
