77b5d43767e37ccdcc5026c49e61a81c787f4699
Gabriel <gabi300709@gmail.com>
2022-08-24 06:47:24 -0300

    renderizado de ofertas guardadas finalizado

--- src/Components/Favorites/CardFavorite/CardFavorite.tsx
+++ src/Components/Favorites/CardFavorite/CardFavorite.tsx
@@ -7,10 +7,8 @@ const CardFavorite = ({props}:any) => {
     const navigate = useNavigate()
 console.log("Card: ", props)
   return (
-    <div >
-        <div className='favorites_DivConteiner' onClick={() => navigate(`/detailOffer/${props.idOffer}`)}>
-            <h4>{props.title}</h4>    
-        </div>
+    <div >   
+      <button className='favorites_btn' onClick={() => navigate(`/detailOffer/${props.idOffer}`)}>{props.title}</button>         
     </div>
   )
 }
