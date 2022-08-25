2d44496baf8032e92f70e1b852d82ac7a76a03d5
Franco Fraiese <86889319+francofraiese@users.noreply.github.com>
2022-08-25 01:07:50 -0300

    si no anda esto yo tiro la toalla

--- src/Components/proposals/CardsProposal/CardsProposal.tsx
+++ src/Components/proposals/CardsProposal/CardsProposal.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import CardProposal from '../CardProposal/CardProposal';
 
 const CardsProposal = ({offer}:any) => {
-  console.log("que me llega???", offer)
   return (
     <div className='Detail_divProposal'>
-      {offer?.proposals?.map((e:any, i:any)=>{
-        <CardProposal key={i} props={e} offer={offer}/>
-        })
-      }
-    </div>
+          {offer.proposals?.map((e:any, i:any)=>{
+            return (
+              <CardProposal key={i} props={e} offer={offer}/>
+            )
+          })}
+        </div>
   )
 }
 
