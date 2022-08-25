f2ad840910614319bb615b4dd2afeb8cfc7b73e4
Franco Fraiese <86889319+francofraiese@users.noreply.github.com>
2022-08-25 04:03:19 -0300

    detalles

--- src/Components/FormPago/PagoResult/Pending.tsx
+++ src/Components/FormPago/PagoResult/Pending.tsx
@@ -20,8 +20,8 @@ const Pending = () => {
       <Header/>
       <div className="pending_cont">
         <div className='pending_display'>
-          <h1>¡Tu pago esta pendiente!</h1>
-          <h2>Estamos teniendo problemas para procesar tu pago</h2>
+          <h1>Tu pago esta pendiente</h1>
+          <h2>Tuvimos problemas para procesar tu pago</h2>
           <h3>Estamos intentando procesar tu pago, esto puede demorar te enviaremos un mail con el resultado.</h3>
           <button className='succes_back' onClick={goHome}>Volver al sitio</button>
         </div>    
