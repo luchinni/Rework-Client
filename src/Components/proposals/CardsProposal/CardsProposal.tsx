b44f314765c5794715eb7a9e9a2859bcc2866bff
Franco Fraiese <86889319+francofraiese@users.noreply.github.com>
2022-08-24 23:40:04 -0300

    la novena es la vencida

--- src/Components/proposals/CardsProposal/CardsProposal.tsx
+++ src/Components/proposals/CardsProposal/CardsProposal.tsx
@@ -1,28 +1,33 @@
 import React from 'react'
 import CardProposal from '../CardProposal/CardProposal';
 
-const CardsProposal = ({offer}:any) => {
+const CardsProposal = ({ offer }: any) => {
 
   return (
     <div className='Detail_divProposal'>
-          {offer.proposals?.map((e:any, i:any)=>{
-            console.log("el state", e.state)
-            console.log("el active", e.isActive)
-            { 
-              (e.state === "cancelled"
+      {offer.proposals?.map((e: any, i: any) => {
+        console.log("el state", e.state)
+        console.log("el active", e.isActive)
+        {
+          (e.state == "cancelled"
             ||
-              e.state === "rejected"
+            e.state == "rejected"
             ||
-              e.state === "contract cancelled" 
+            e.state == "contract cancelled"
             ||
-              e.state === "contract rejected")
-            ? 
+            e.state == "contract rejected")
+            ?
+            <div>
               <h1>ENTRE AL CANCEL</h1>
-            : 
-              <CardProposal key={i} props={e} offer={offer}/> 
-          }
-          })}
-        </div>
+            </div>
+            :
+            <div>
+              <CardProposal key={i} props={e} offer={offer} />
+            </div>
+
+        }
+      })}
+    </div>
   )
 }
 
