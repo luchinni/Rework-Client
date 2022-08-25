e830502cf846e9e8edab44a01ce4e1ddbb401e1b
Franco Fraiese <86889319+francofraiese@users.noreply.github.com>
2022-08-25 00:14:12 -0300

    console log

--- src/Components/proposals/CardsProposal/CardsProposal.tsx
+++ src/Components/proposals/CardsProposal/CardsProposal.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import CardProposal from '../CardProposal/CardProposal';
 
 const CardsProposal = ({offer}:any) => {
-
+  console.log("que me llega???", offer)
   return (
     <div className='Detail_divProposal'>
       {offer.proposals?.map((e:any, i:any)=>{
