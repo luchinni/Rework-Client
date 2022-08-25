c989708ddbb4bb6f3be65a25a10541da895974b3
lucas alejandro pardieux <lucaspardieux@gmail.com>
2022-08-25 09:19:50 -0300

    css premium

--- src/Components/FormPago/PagoPremium/SeleccionPremium.tsx
+++ src/Components/FormPago/PagoPremium/SeleccionPremium.tsx
@@ -4,6 +4,8 @@ import logo from "../../../images/logo_header.png"
 import logo_premium from "../../../images/logo_header_premium.png"
 import Header from '../../Header/Header'
 import "./SeleccionPremium.css"
+import {GiCheckMark} from "react-icons/gi"
+
 
 const SeleccionPremium = () => {
 
@@ -20,29 +22,33 @@ const SeleccionPremium = () => {
     <div>
         <Header></Header>
         <div className='Premium_all_cont'>
-        <h1>¡Actualiza tu cuenta y accede a mas beneficios!</h1>
+        <h1><b>¡Actualiza tu cuenta y a</b>ccede a mas beneficios!</h1>
         <div className='Premium_cont'>
         <div className='Premium_each_cont'>
             <h2>Cuenta gratuita</h2>
-            <img src={logo} alt="" />
+            <img className='free_image' src={logo} alt="" />
             <ul className='Premium_ul'>
-                <li><p> Recibe el pago en un plazo de 5 a 10 dias al finalizar tus trabajos.</p></li>
-                <li><p> Compite a ciegas contra otros trabajadores por el puesto.</p></li>
-                <li><p> Envia propuestas una unica vez por oferta.</p></li>
+                <li><p><GiCheckMark className='check_icon'/> Recibe el pago en un plazo de 5 a 10 dias al finalizar tus trabajos.</p></li>
+                <br />
+                <li><p><GiCheckMark className='check_icon'/> Compite a ciegas contra otros trabajadores por el puesto.</p></li>
+                <br />
+                <li><p><GiCheckMark className='check_icon'/> Envia propuestas una unica vez por oferta.</p></li>
             </ul>
-            <button className="CR_inputSubmit" onClick={freeAcount}>Continuar gratis</button>
+            <button className="Submit_button_premium" onClick={freeAcount}>Continuar gratis</button>
         </div>
         <div className='Premium_each_cont'>
             <h2>Cuenta Premium</h2>
             <div className='Premium_topBox'>
-                <img src={logo_premium} alt="" />
+                <img className='Premium_image' src={logo_premium} alt="" />
             </div>
             <ul className='Premium_ul'>
-                <li><p> Recibe el pago en un plazo de 72hs máximo al finalizar tus trabajos.</p></li>
-                <li><p> Revisa las propuestas de tus competidores y compite con ventaja.</p></li>
-                <li><p> Edita tus propuestas y mejora tus posibilidades</p></li>
+                <li><p><GiCheckMark className='check_icon'/> Recibe el pago en un plazo de 72hs máximo al finalizar tus trabajos.</p></li>
+                <br />
+                <li><p><GiCheckMark className='check_icon'/> Revisa las propuestas de tus competidores y compite con ventaja.</p></li>
+                <br />
+                <li><p><GiCheckMark className='check_icon'/> Edita tus propuestas y mejora tus posibilidades</p></li>
             </ul>
-            <button className="CR_inputSubmit" onClick={premiumAcount}>Actualizar a premium</button>
+            <button className="Submit_button_premium" onClick={premiumAcount}>Actualizar a premium</button>
         </div>
         </div>
         
