ba853a43e76ba9d4694245ca5877eecfaf87c1bc
Franco Fraiese <86889319+francofraiese@users.noreply.github.com>
2022-08-25 00:01:37 -0300

    asd

--- src/Components/proposals/CardsProposal/CardsProposal.tsx
+++ src/Components/proposals/CardsProposal/CardsProposal.tsx
@@ -5,16 +5,11 @@ const CardsProposal = ({offer}:any) => {
 
   return (
     <div className='Detail_divProposal'>
-          {offer.proposals?.map((e:any, i:any)=>{
-            e.isActive === true ?
-            (
-                <CardProposal key={i} props={e} offer={offer}/>
-            ) 
-            :
-              <br/>  
-
-          })}
-        </div>
+      {offer.proposals?.map((e:any, i:any)=>{
+        <CardProposal key={i} props={e} offer={offer}/>
+        })
+      }
+    </div>
   )
 }
 
