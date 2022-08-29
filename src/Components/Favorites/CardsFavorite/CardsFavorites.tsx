23ea4b6534dd81efa466abcc3bdeb8690407d1f7
Gabriel <gabi300709@gmail.com>
2022-08-29 17:34:36 -0300

    avance sobre delete favorites en home

--- src/Components/Favorites/CardsFavorite/CardsFavorites.tsx
+++ src/Components/Favorites/CardsFavorite/CardsFavorites.tsx
@@ -1,13 +1,27 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import CardFavorite from '../CardFavorite/CardFavorite';
-
+import { getUserById, remFavoritestoDB } from '../../../Redux/Reducer/reducer';
+import decode from "jwt-decode"
 import './CardsFavorites.css';
 
 
 const CardsFavorites = ({favoriteInfo}:any) => {
-  console.log(favoriteInfo)
+
   const currentUser = useSelector((state:any) => state.workService.currentUser);
+  const userLogged = useSelector((state:any) => state.workService.userLogged);
+  const dispatch = useDispatch()
+  const token:any = localStorage.getItem("token")
+  let tokenDecode:any = decode(token)
+
+  async function onClose (favoriteInfo:any){
+    if(userLogged.favorites?.find((f:any) => f.idOffer === favoriteInfo.idOffer)){
+      await remFavoritestoDB(favoriteInfo, userLogged.id);
+      return dispatch(getUserById(tokenDecode))
+    }
+  }
+
+
   return (
     <div className='CardsFavorites_component'>
       <div className='Favorites_title'>
@@ -21,7 +35,7 @@ const CardsFavorites = ({favoriteInfo}:any) => {
       <div className='CardFavorite_component'>
             {
               favoriteInfo && favoriteInfo?.map((o:any, i:any) =>{
-          return  <CardFavorite key={i} props={o} />
+          return  <CardFavorite key={i} props={o} onClose={onClose} />
         })
 
       }     
